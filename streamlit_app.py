aa3646144e1bc2f00a83dbfbc8b39b6775857d71
vicsoliveira <116915472+vicsoliveira@users.noreply.github.com>
2024-06-14 16:35:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,9 @@ if uploaded_excel is not None and uploaded_word is not None:
     # Define the mapping from Excel to Word placeholders based on inspection
     data_mapping = {
         "{{Nome do município}}": df.iloc[0, df.columns.get_loc("Unnamed: 1")],
-        "{{População residente}}": df.iloc[6, df.columns.get_loc("Unnamed: 1")],
-        "{{Área da unidade territorial}}": df.iloc[7, df.columns.get_loc("Unnamed: 1")],
-        "{{Densidade demográfica}}": df.iloc[8, df.columns.get_loc("Unnamed: 1")],
+        "{{População residente}}": df.iloc[6, df.columns.get_loc("População residente (Pessoas)")],
+        "{{Área da unidade territorial}}": df.iloc[7, df.columns.get_loc("Área da unidade territorial (Quilômetros quadrados)")],
+        "{{Densidade demográfica}}": df.iloc[8, df.columns.get_loc("Densidade demográfica (Habitante por quilômetro quadrado)")],
         "{{Área total}}": df.iloc[12, df.columns.get_loc("Área total do estabelecimento agropecuário")],
         "{{Plantio em nível}}": df.iloc[13, df.columns.get_loc("Plantio em nível")],
         "{{Rotação de culturas}}": df.iloc[14, df.columns.get_loc("Rotação de culturas")],
@@ -57,12 +57,12 @@ if uploaded_excel is not None and uploaded_word is not None:
         "{{Estabilização de voçorocas}}": df.iloc[19, df.columns.get_loc("Estabilização de voçorocas")],
         "{{Manejo florestal}}": df.iloc[20, df.columns.get_loc("Manejo florestal")],
         "{{Outras}}": df.iloc[21, df.columns.get_loc("Outras")],
-        "{{PIB}}": df.iloc[26, df.columns.get_loc("Unnamed: 1")],
-        "{{Percentual da agricultura}}": df.iloc[27, df.columns.get_loc("Unnamed: 1")],
-        "{{Valor Adicionado Bruto Agropecuária}}": df.iloc[34, df.columns.get_loc("Unnamed: 1")],
-        "{{Valor Adicionado Bruto Indústria}}": df.iloc[35, df.columns.get_loc("Unnamed: 1")],
-        "{{Valor Adicionado Bruto Serviços}}": df.iloc[36, df.columns.get_loc("Unnamed: 1")],
-        "{{Valor Adicionado Bruto Administração Pública}}": df.iloc[37, df.columns.get_loc("Unnamed: 1")]
+        "{{PIB}}": df.iloc[26, df.columns.get_loc("Produto Interno Bruto - PIB (Mil R$)")],
+        "{{Percentual da agricultura}}": df.iloc[27, df.columns.get_loc("Percentual da agricultura no PIB")],
+        "{{Valor Adicionado Bruto Agropecuária}}": df.iloc[34, df.columns.get_loc("Valor Adicionado Bruto Agropecuária")],
+        "{{Valor Adicionado Bruto Indústria}}": df.iloc[35, df.columns.get_loc("Valor Adicionado Bruto Indústria")],
+        "{{Valor Adicionado Bruto Serviços}}": df.iloc[36, df.columns.get_loc("Valor Adicionado Bruto Serviços")],
+        "{{Valor Adicionado Bruto Administração Pública}}": df.iloc[37, df.columns.get_loc("Valor Adicionado Bruto Administração Pública")]
     }
 
     # Replace placeholders in the Word document
